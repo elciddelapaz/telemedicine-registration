79b5ae45bc6e80f6a141398bc387f3c11dfd508e
Nico <gabo.janicolo@gmail.com>
2022-09-15 14:03:11 +0800

    Update App.js

--- telemedicine-frontend/src/App.js
+++ telemedicine-frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Registration from './components/registration/Registration';
+import TermsAndConditions from './components/termsAndConditions/TermsAndConditions';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Registration />
+      <TermsAndConditions />
       <Footer />
     </div>
   );
